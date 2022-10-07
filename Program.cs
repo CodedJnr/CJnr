c91812bd8ee2aa8676c855ef8f67d08248222b38
BAKARE OLAWALE <jnrcoded14@gmail.com>
2022-10-07 10:35:02 +0100

    FirstGitCommit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Full_Stack_Legendary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program to check for complete prime
+            // Console.Write("Enter number : ");
+            // int num = int.Parse(Console.ReadLine());
+            // bool isCompletePrime = true;
+            // while(num>0&&isCompletePrime)
+            // {
+            //     int lastNum = num%10;
+            //     if(num<2) 
+            //     {
+            //         isCompletePrime = false;
+            //         break;
+            //     }
+            //     bool isPrime = true;
+            //     for(int i=2;i<lastNum;i++)
+            //     {
+            //         if(lastNum%i==0) isPrime = false;
+            //         break;
+            //     }
+            //     isCompletePrime=isPrime;
+            //     num = num/10;
+            // }
+            // Console.WriteLine(isCompletePrime ? "Number is Complete Prime" : "Number is NOT Complete Prime");
+
+            
+        }
+    }
+}
